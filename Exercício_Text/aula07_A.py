1f85c9503d628b794188c1a2c8f73b759d18c2dd
JoshuaBertocchi <joshuabertocchi@gmail.com>
2024-08-15 06:41:00 -0300

    Aula_Curso_Guanabara

--- Exercício_Text/aula07_A.py
+++ Exercício_Text/aula07_A.py
@@ -1,2 +1,11 @@
-nome=input('Qual é seu nome?')
-print(f'Prazer em lhe conhecer {nome:<15}!')
+n1=int(input('Digite um valor:'))
+n2=int(input('Digite outro valor:'))
+s=n1+n2
+sub=n1-n2
+m=n1*n2
+d=n1/n2
+p=n1**n2
+div=n1//n2
+res=n1%n2
+print(f'A soma é: {s}\n O Produto é {m}\n A Divisão é: {d:.3f}\n A Potência é: {p}', end=' ___ ')
+print(f'Divisão inteira é: {div:.3f}\n Resto da divisão: {res}')
---
+++ Exercício_Text/aula07_B.py
@@ -0,0 +1,4 @@
+n1=int(input('Digite um número inteiro:'))
+n2=n1+1
+n3=n1-1
+print(f' O número foi:{n1} \n O antecessor é: {n3} \n O sucessor é:{n2}')
