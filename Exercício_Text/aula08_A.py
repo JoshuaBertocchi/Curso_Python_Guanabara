ca6ec66de83234881e531cb4b1ed31fdcf7cbf71
JoshuaBertocchi <joshuabertocchi@gmail.com>
2024-08-20 06:11:32 -0300

    Aula_Curso_Guanabara

---
+++ Exercício_Text/aula08_A.py
@@ -0,0 +1,4 @@
+import math
+num = int(input('Digite um número e veja sua raiz: '))
+raiz = math.sqrt(num)
+print (f'A raiz de {num} é {round(raiz)}')
---
+++ Exercício_Text/aula08_B.py
@@ -0,0 +1,3 @@
+import random, emoji
+num = random.randint(1,10)
+print(emoji.emojize(f'Valor {num} :cookie:'))
