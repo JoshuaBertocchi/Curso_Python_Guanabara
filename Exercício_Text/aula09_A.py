125a23830fe03a568fcef73a58ae8c71c2c977d6
JoshuaBertocchi <joshuabertocchi@gmail.com>
2024-08-25 13:03:55 -0300

    Aula_Curso_Guanabara

---
+++ Exercício_Text/aula09_A.py
@@ -0,0 +1,8 @@
+frase= 'Curso em Vídeo Python!'
+print(frase[15:22])
+print(len(frase))
+print(frase.replace('Curso', 'Aula'))
+print(frase)
+frase01= '   Curso em Vídeo Python!!!   '
+print(len(frase01))
+print(frase01.strip())
