72fb20d820537dd68ee4f231abac21d0a891912c
JoshuaBertocchi <joshuabertocchi@gmail.com>
2024-09-05 07:50:34 -0300

    Aula_Curso_Guanabara

---
+++ Exercício_aula_final/ex036_código_teste.py
@@ -0,0 +1,20 @@
+from time import sleep
+''''print('=$='*8,'Banco do Bertocchi','=$='*8)
+
+print('Seja bem-vindo ao banco.!')
+
+v_casa_str= input('Qual é o valor da casa?')
+v_casa_convert= v_casa_str.replace(',','.')
+v_casa_float=float(v_casa_convert)
+print(v_casa_float)
+
+
+print('Salvando Dados...')
+sleep(3)
+
+
+salário=float(input('Quanto você recebe por mês?'))
+
+print('Salvando Dados...')
+sleep(3)'''
+
